37cb5ef2495fd5af25b9b9bd91b76d1e5dce619e
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-01-18 21:56:32 -0300

    Create pagamento.js

---
+++ pagamento.js
@@ -0,0 +1,11 @@
+let valor1 = parseInt(gets(this.valor1));
+
+let valor2 = parseInt(gets(this.valor2));
+
+let valor3 = parseFloat(gets(this.valor3));
+
+let salary = parseFloat(valor2 * valor3).toFixed(2); // Digite aqui o calculo do salário
+
+console.log("NUMBER = " + valor1);
+
+console.log("SALARY = U$ " + salary);
